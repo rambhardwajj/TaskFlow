aed6f15b1032c1c82fd0941d060cdfb65f32f88c
rambhardwajj <rambhardwaj101000@gmail.com>
2025-05-26 20:58:07 +0530

    Routes

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,13 +1,25 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import ProjectPage from "./pages/ProjectPage";
+import Projects from "./pages/Projects";
+import {Layout} from "./pages/Layout";
+import TasksOfProject from "./pages/TasksOfProject";
+import {Dashboard} from "./pages/Dashboard";
+import CreateProjectPage from "./pages/CreateProject";
+import MyTasks from "./pages/MyTasks";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/projectId" element={<ProjectPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/create" element={<CreateProjectPage />} />
+          <Route path=":projectId/tasks" element={<TasksOfProject />} />
+          <Route path="tasks" element={<MyTasks />} />
+          <Route index element={<Dashboard />} />
+          {/* <Route path=":projectId" element={<ProjectPage />} /> */}
+
+        </Route>
       </Routes>
     </Router>
   );
