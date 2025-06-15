cc1a3ecaa0887b56fd1f072e1c1270ca10b0bf6b
rambhardwajj <rambhardwaj101000@gmail.com>
2025-06-15 15:23:50 +0530

    adding cors

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -6,12 +6,13 @@ import projectRouter from "./routes/project.routes";
 import taskRouter from "./routes/task.routes";
 import noteRouter from "./routes/note.routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import { envConfig } from "./configs/env";
 
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
+app.use(cors({ credentials: true, origin: ["http://localhost:5173", envConfig.APP_URL] }));
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/project", projectRouter);
